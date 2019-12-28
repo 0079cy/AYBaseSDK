933c16b6f86b4b825f316cdba6dcbde29e9627a7
chenyan4 <chenyan4@ffcs.cn>
2019-12-28 16:36:42 +0800

    test

---
+++ build/AYBaseSDK.framework/Headers/AYBaseSDK.h
@@ -0,0 +1,19 @@
+//
+//  AYBaseSDK.h
+//  AYBaseSDK
+//
+//  Created by 陈䶮 on 2019/12/28.
+//  Copyright © 2019 陈䶮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AYBaseSDK.
+FOUNDATION_EXPORT double AYBaseSDKVersionNumber;
+
+//! Project version string for AYBaseSDK.
+FOUNDATION_EXPORT const unsigned char AYBaseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AYBaseSDK/PublicHeader.h>
+
+
